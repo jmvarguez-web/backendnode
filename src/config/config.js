8704715df01b2c9946b94131f061ac3dd45d3d46
jmvarguez <jmvarguez@hotmail.com>
2024-01-05 20:55:50 -0600

    modificando variables

--- src/config/config.js
+++ src/config/config.js
@@ -13,7 +13,6 @@ import 'dotenv/config'
     dbPassword:process.env.DB_PASSWORD || 'user2023.MY2023',
     dbHost:process.env.DB_HOST || 'localhost',
     dbName:process.env.DB_NAME || 'libreria',
-    dbdockerPort:process.env.MYSQLDB_DOCKER_PORT || 3306,
     dbPort:process.env.DB_PORT || 3309,
     keyacces:process.env.KEYACCES}
 
